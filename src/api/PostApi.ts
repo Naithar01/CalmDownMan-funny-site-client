067e5ff5066ba7ca34f565ad2bc05259723d3650
Naithar01 <eeung025@gmail.com>
2022-12-11 00:47:37 +0900

    Edit Header Layout, Add Main Page & Api

---
+++ src/api/PostApi.ts
@@ -0,0 +1,22 @@
+export type Post = {
+  id: number;
+  title: string;
+  content: number;
+  category_id: number;
+  author_id: number;
+  created_at: string | Date;
+  updated_at: string | Date;
+};
+
+export const GetMainPagePost = async () => {
+  return await fetch("http://localhost:8080/api/post/", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data.datas;
+    });
+};
--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,10 +1,43 @@
-import style from "../styles/page/main.module.css";
+import { useEffect, useState } from "react";
+import { GetMainPagePost, Post } from "../api/PostApi";
+import MainPagePostList from "../components/MainPage/PostList";
+import style from "../styles/pages/main.module.css";
 
 const MainPage = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    GetPosts();
+  }, []);
+
+  const GetPosts = async () => {
+    const datas: Post[] = await GetMainPagePost();
+
+    setPosts(datas);
+  };
+
   return (
-    <div className={style.main_page}>
-      <p>Hello World</p>
-    </div>
+    <main className={style.main_page}>
+      <section className="left"></section>
+      <section className={style.main_page_list}>
+        {/* Title */}
+        <section className={style.main_page_list_title}>
+          Main Page Post Category Name
+        </section>
+        {/* Notice */}
+        <section
+          id="notice"
+          className={style.main_page_list_post_list}
+        ></section>
+        {/* Post List */}
+        <section className={style.main_page_list_post_list}>
+          {posts &&
+            posts.length &&
+            posts.map((post) => <MainPagePostList key={post.id} post={post} />)}
+        </section>
+      </section>
+      <section></section>
+    </main>
   );
 };
 
